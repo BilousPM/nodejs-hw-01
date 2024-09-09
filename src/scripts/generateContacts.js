836aa43e7bbed28ecf6af315e12f2d70dd274003
BilousPM <bilouspm@gmail.com>
2024-09-08 22:28:56 -0600

    add functions to add and delete contacts to the DataBase

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,18 @@
-const generateContacts = async (number) => {};
+import fs from 'node:fs/promises';
+import { PATH_DB } from '../constants/contacts.js';
+import { readContacts } from '../utils/readContacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+
+const generateContacts = async (number) => {
+  try {
+    const contacts = await readContacts();
+    for (let i = 1; i <= number; i++) {
+      contacts.push(createFakeContact());
+    }
+    fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2), 'utf-8');
+  } catch (e) {
+    console.log(e);
+  }
+};
 
 generateContacts(5);
